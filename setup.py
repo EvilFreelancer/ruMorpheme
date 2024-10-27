b7962eec69af39e64f1d0fe45d2adc6a33e1647e
pasha <paul@drteam.rocks>
2024-10-27 16:41:01 +0300

    Small tunes for publication on pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ruMorpheme',
+    version='0.1.0',
+    author='Pavel Rykov',
+    author_email='paul@drteam.rocks',
+    description='Проект языковой модели для проведения морфемного анализа и сегментации слов русского языка.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/EvilFreelancer/ruMorpheme',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        "numpy~=2.1.1",
+        "torch~=2.4.1",
+        "huggingface-hub~=0.25.1",
+    ],
+)
